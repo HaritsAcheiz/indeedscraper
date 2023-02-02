d2d4d30dde1a9f78c01d7feecb73a4636a9c5f5e
HaritsAcheiz <harits.muhammad.only@gmail.com>
2023-02-03 04:38:23 +0700

    innit commit, cf_clearance not found

--- main.py
+++ main.py
@@ -1,54 +1,84 @@
 # import httpx
 import asyncio
+
+import cloudscraper
+import httpx
 from selectolax.parser import HTMLParser
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 
-if __name__ == '__main__':
+def webdriver_setup(proxy=None,useragent=None):
+    if proxy != None:
+        split_proxy = proxy.split(':')
+        ip = split_proxy[0]
+        port = split_proxy[1]
 
-    url = 'https://de.indeed.com/jobs?q=Junior+Sales&l=Berlin&start=0'
+        ff_opt = Options()
 
-    ip = '154.12.198.179'
-    port = '8800'
-    useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0"
+        # ff_opt.add_argument("--headless")
+        ff_opt.add_argument("--no-sandbox")
+        # ff_opt.set_preference("general.useragent.override", useragent)
+        ff_opt.set_preference('network.proxy.type', 1)
+        ff_opt.set_preference('network.proxy.socks', ip)
+        ff_opt.set_preference('network.proxy.socks_port', int(port))
+        ff_opt.set_preference('network.proxy.socks_version', 5)
+        ff_opt.set_preference('network.proxy.socks_remote_dns', True)
+        ff_opt.set_preference('network.proxy.http', ip)
+        ff_opt.set_preference('network.proxy.http_port', int(port))
+        ff_opt.set_preference('network.proxy.ssl', ip)
+        ff_opt.set_preference('network.proxy.ssl_port', int(port))
 
-    header = {#"Host": "de.indeed.com",
-              "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
-              # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-              # "Accept-Language": "en-US,en;q=0.5",
-              # "Accept-Encoding": "gzip, deflate, br",
-              # "Alt-Used": "de.indeed.com",
-              # "Connection": "keep-alive",
-              "Cookie": '__cf_bm=RnnLvi6GZeFGdCfLDuPGPvZXaNUrAiYramx.Bd70Z4M-1675112221-0-ASShsXuGJmVJJTL4j+xgHdozQKaKxHlL6JaCaJCdOwYeujUO2e6aTDRUATKyJMttSO/R9owRFRD8yULaOscOgWw=; _cfuvid=wOSj942v1TUrAfl8KS2ZoUK8R6TJ0Z5GhGDl5Y2BoK0-1675110204040-0-604800000; CTK=1go24a40nmjlk800; RF="QXOA6F_NPhPqwOrnO1efjyOe0CXJrE4IgGyhxIXU9NqQsR8bwun4h4WUCXf66hD8AFFrKkP89Do="; CSRF=mxIMRoRM9Ug5MzqBSxq9V6VU1o8KAl8E; SURF=YH99BIa5dLcjgE2GXNDItk6mL9On12Nx; _ga=GA1.2.1505812215.1675108885; _gid=GA1.2.421615850.1675108885; gonetap=closed; g_state={"i_p":1675116087327,"i_l":1}; _gcl_au=1.1.345893952.1675109241; SHARED_INDEED_CSRF_TOKEN=46GDLXfzK02YgoHniK5qBG9db78McrfS; LC="co=ID"; CO=DE; LOCALE=de; MICRO_CONTENT_CSRF_TOKEN=AzjpvrByVmoEFB9l7JJhhGmsfkHWLgKk; PPID=""; _mkto_trk=id:699-SXJ-715&token:_mch-indeed.com-1675110287086-29309; CO=US; LOCALE=en; indeed_rcc="LOCALE:PREF:LV:CTK:CO:UD"; INDEED_CSRF_TOKEN=zBX53OFWYFJbQi6A5FSVAWzRtCk5llR4; LV="LA=1675112369:CV=1675110308:TS=1675110291"; UD="LA=1675112369:CV=1675110308:TS=1675110308:SG=2749b5e7911f94f8d7238b020d444006"; PREF="TM=1675110308079:LD=de:L=Berlin"; LANG=de; OptanonConsent=isGpcEnabled=0&datestamp=Tue+Jan+31+2023+03%3A59%3A34+GMT%2B0700+(Indochina+Time)&version=202210.1.0&isIABGlobal=false&hosts=&consentId=640cd21f-98b8-40a2-b572-38fecf793323&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A0%2CC0003%3A0%2CC0004%3A0%2CC0007%3A0&AwaitingReconsent=false; JSESSIONID=F3AA139B4DC7DD029535D62EE369EF59; jaSerpCount=13; RQ="q=Junior+Sales&l=Berlin&ts=1675112369962"; ac=/2KKwKDgEe2Q2lkn+ZIULA#/2NOEKDgEe2Q2lkn+ZIULA',
-              # "Upgrade-Insecure-Requests": "1",
-              # "Sec-Fetch-Dest": "document",
-              # "Sec-Fetch-Mode": "navigate",
-              # "Sec-Fetch-Site": "none",
-              # "Sec-Fetch-User": "?1",
-              # "TE": "trailers"
-              }
+    else:
+        ff_opt = Options()
 
-    # with httpx.Client(proxies=proxies, headers=header, default_encoding='utf-8', verify=False) as client:
-    #     html = client.get(url)
-    # print(html)
-
-    ff_opt = Options()
-
-    # ff_opt.add_argument("--headless")
-    ff_opt.add_argument("--no-sandbox")
-    ff_opt.set_preference("general.useragent.override", useragent)
-    ff_opt.set_preference('network.proxy.type', 1)
-    ff_opt.set_preference('network.proxy.socks', ip)
-    ff_opt.set_preference('network.proxy.socks_port', int(port))
-    ff_opt.set_preference('network.proxy.socks_version', 4)
-    ff_opt.set_preference('network.proxy.socks_remote_dns', True)
-    ff_opt.set_preference('network.proxy.http', ip)
-    ff_opt.set_preference('network.proxy.http_port', int(port))
-    ff_opt.set_preference('network.proxy.ssl', ip)
-    ff_opt.set_preference('network.proxy.ssl_port', int(port))
-
+        ff_opt.add_argument("--no-sandbox")
 
     driver = Firefox(options=ff_opt)
+
+    return driver
+
+def get_cookies(driver, url):
     driver.get(url)
     driver.maximize_window()
-    print(driver.current_url)
+
+    result = {'ua':None, 'cookies':None}
+    # get cookies and useragent
+    brCookies = driver.get_cookies()
+    ua = driver.execute_script('return navigator.userAgent')
+    result['ua'] = {"user-agent": ua}
+
+    # driver.quit()
+
+    # filter out the cf_clearance cookie
+    cf_cookies = [cookie for cookie in brCookies if cookie['name'] in ('__cf_bm', '_cfuvid')]
+    result['cookies'] = {"__cf_bm": cf_cookies[0]['value'],
+               "_cfuvid": cf_cookies[1]['value']
+               }
+    return result
+
+def get_pages(url, user_agent, cookies, proxy):
+    proxies = {
+        "http://": f"http://{proxy}",
+        "https://": f"http://{proxy}"
+    }
+    with httpx.Client(headers=user_agent, cookies=cookies, proxies=proxies) as client:
+        html_pages = client.get(url).text
+    return html_pages
+
+# def get_pages_cs(url):
+#     with cloudscraper.create_scraper() as scraper:
+#         html_pages = scraper.get(url).text
+#     return html_pages
+def main():
+    url = 'https://de.indeed.com/jobs?q=Junior+Sales&l=Berlin&start=0'
+    driver = webdriver_setup(proxy='192.126.253.59:8800')
+    cookies = get_cookies(driver=driver, url=url)
+    result = get_pages(url=url, user_agent=cookies['ua'], cookies=cookies['cookies'], proxy='192.126.253.59:8800')
+    # result2 = get_pages_cs(url=url)
+    print('=======================result============================')
+    print(result)
+    # print('=======================result2============================')
+    # print(result2)
+
+if __name__ == '__main__':
+    main()
